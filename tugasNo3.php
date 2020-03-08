e1f3b1e5a4f8758ab6ba68542e717c56753ef3dc
Diah Fatmawati <61172685+diahfatmawati@users.noreply.github.com>
2020-03-08 19:00:06 +0700

    Tugas Praktikum 5 : Program Sederhana Biodata PHP
    Tugas Praktikum 5 : PHP GET POST.
    Program sederhana BIodata dengan PHP dan HTML.
    
    Diah Fatmawati
    18082010012

---
+++ tugasNo3.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Biodata Diri Diah Fatmawati </title>
+</head>
+<body>
+    <h1 align="center"> Biodata Diah Fatmawati </h1>
+    <img src="me.jpg" width="150px" height="250px" style="display: block; margin: auto;">
+    <br>
+<?php
+echo "<center>";
+echo "<table width='600' border='0'>";
+  echo "<tr>";
+    echo "<td width='30'>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td width='200'>";
+        echo "Nama Lengkap";
+        echo "</td>";
+    echo "<td width='10'>";
+        echo ":";
+        echo "</td>";
+    echo "<td width='250'>";
+        echo "Diah Fatmawati";
+        echo "</td>";
+  echo "</tr>";
+
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Nama Panggilan";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Diah";
+        echo "</td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+      
+    echo "<td>";
+        echo "Jenis Kelamin";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Perempuan";
+        echo "</td>";
+  echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Agama";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Islam";
+        echo "</td>";
+  echo "</tr>";
+
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Tempat, tanggal lahir";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Surabaya, 19 Juli 2000";
+        echo "</td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+      
+    echo "<td>";
+        echo "Pendidikan";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Mahasiswa";
+        echo "</td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+      
+    echo "<td>";
+        echo "Telepon";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "083857667563";
+        echo "</td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+      
+    echo "<td>";
+        echo "Alamat Email";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "fadiahachmad@gmail.com";
+        echo "</td>";
+  echo "</tr>";
+
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Status";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Belum Menikah";
+        echo "</td>";
+  echo "</tr>";
+
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Kewarganegaraan";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Indonesia";
+        echo "</td>";
+  echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Hobi";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Membaca dan Menulis Cerpen";
+        echo "</td>";
+
+  echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Pendidikan Formal";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "SDN Manukan Kulon VI Surabaya, lulus tahun 2012";
+        echo "</td>";
+  echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "";
+        echo "</td>";
+    echo "<td>";
+        echo "";
+        echo "</td>";
+    echo "<td>";
+        echo "SMP Negeri 33 Surabaya, lulus tahun 2015";
+        echo "</td>";
+  echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "";
+        echo "</td>";
+    echo "<td>";
+        echo "";
+        echo "</td>";
+    echo "<td>";
+        echo "SMA IT Shafta Surabaya, lulus tahun 2018";
+        echo "</td>";
+  echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "";
+        echo "</td>";
+    echo "<td>";
+        echo "";
+        echo "</td>";
+    echo "<td>";
+        echo "UPN 'Veteran' Jawa Timur, masuk tahun 2018 ";
+        echo "</td>";
+  echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Pengalaman Kerja";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Guru Les Privat, tahun 2015 - sekarang";
+        echo "</td>";
+  echo "</tr>";
+echo "</table>";
+?>
+</body>
+</html>
